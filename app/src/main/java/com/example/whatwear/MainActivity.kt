0838cb5af1c6d2c732cc01b8005dc8494b20764c
nahed <nahedshehata>
2023-04-18 20:05:57 +0200

    complete project

--- app/src/main/java/com/example/whatwear/MainActivity.kt
+++ app/src/main/java/com/example/whatwear/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.whatwear
 
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.whatwear.databinding.ActivityMainBinding
 import com.google.gson.Gson
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 import java.net.URL
 
@@ -15,13 +19,14 @@ private const val BASE_URL =
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    lateinit var binding: ActivityMainBinding
-   private lateinit var client: OkHttpClient
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var client: OkHttpClient
     private lateinit var sharedPreferences: SharedPreferences
-    lateinit var myEdit: SharedPreferences.Editor
-    var winterArr = listOf(R.drawable.sweet1, R.drawable.sweet2, R.drawable.bamb)
-    var summerArr =
+    private lateinit var myEdit: SharedPreferences.Editor
+    private var winterArr = listOf(R.drawable.sweet1, R.drawable.sweet2, R.drawable.bamb)
+    private var summerArr =
         listOf(R.drawable.dress1, R.drawable.dress2, R.drawable.dress3, R.drawable.drees4)
+    private var moodArr= listOf(R.drawable.rain,R.drawable.sunwith,R.drawable.sun)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,59 +42,62 @@ class MainActivity : AppCompatActivity() {
             makeRequest(BASE_URL)
         }
     }
-
     private fun makeRequest(sUrl: String): WeatherResponse? {
         var result: WeatherResponse? = null
         try {
             val url = URL(sUrl)
-
             val request = Request.Builder().url(url).build()
+             client.newCall(request).enqueue(object : Callback {
 
-            val response = client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        Log.e(TAG, "onFailure: ${e.localizedMessage}")
-                    }
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e(TAG, "onFailure: ${e.localizedMessage}")
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    result = response.body?.string()?.toKotlinObject<WeatherResponse>()
+                    val temperature =
+                        result?.data?.timelines?.get(0)?.intervals?.get(0)?.values?.temperature
 
-                    override fun onResponse(call: Call, response: Response) {
-                        result = response.body?.string()?.toKotlinObject<WeatherResponse>()
-                        val temperature =
-                            result?.data?.timelines?.get(0)?.intervals?.get(0)?.values?.temperature
-                        Log.d(
-                            TAG,
-                            "makeRequest: temperature: $temperature"
-                        )
-                        if (response.isSuccessful) {
-                            runOnUiThread {
-                                binding.tvTemperatureValue.text = "$temperature°C"
+                    if (response.isSuccessful) {
+                        runOnUiThread {
+                            binding.tvTemperatureValue.text = "$temperature°C"
 
-                                temperature?.let {
-                                    if (temperature > 25) {
-                                        var index = sharedPreferences.getInt("summer", 0)
-                                        binding.ivWear.setImageResource(summerArr[index])
-                                        index++
-                                        if (index > 3)
-                                            index = 0
-                                        myEdit.putInt("summer", index)
-                                        myEdit.commit()
+                            temperature?.let {
+                                if (temperature > 25) {
+                                    summer()
 
-                                    } else {
-                                        var index = sharedPreferences.getInt("winter", 0)
-                                        binding.ivWear.setImageResource(winterArr[index])
-                                        index++
-                                        if (index > 2)
-                                            index = 0
-                                        myEdit.putInt("winter", index)
-                                        myEdit.commit()
-                                    }
+
+                                } else {
+                                    winter()
                                 }
                             }
                         }
                     }
-                })
+                }
+            })
         } catch (err: Error) {
             print("Error when executing get request: " + err.localizedMessage)
         }
         return result
+    }
+   private fun summer(){
+        var index = sharedPreferences.getInt("summer", 0)
+        binding.ivWear.setImageResource(summerArr[index])
+       binding.ivMood.setImageResource(moodArr[2])
+        index++
+        if (index > 3)
+            index = 0
+        myEdit.putInt("summer", index)
+        myEdit.commit()
+    }
+   private fun  winter(){
+       var index = sharedPreferences.getInt("winter", 0)
+       binding.ivWear.setImageResource(winterArr[index])
+       binding.ivMood.setImageResource(moodArr[0])
+       index++
+       if (index > 2)
+           index = 0
+       myEdit.putInt("winter", index)
+       myEdit.commit()
     }
 
     inline fun <reified T : Any> String.toKotlinObject(): T =
